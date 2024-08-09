dd00965fd5e3d9578f834c99b512deaa84b2f0c9
souravs3 <sourv1230@gmail.com>
2024-08-09 05:44:56 -0700

    first commit

---
+++ lib/Utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const firstColor = Color.fromARGB(255, 94, 52, 28);
+const secondColor = Color.fromARGB(255, 81, 51, 29);
+const thirdColor = Color.fromARGB(255, 168, 109, 72);
